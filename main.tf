7107738270daeeeb8e8a56166f712b72975ae9c0
NitishChandu <nitish.chandu@gmail.com>
2022-07-21 16:01:05 +0530

    updated proj details

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "jenkins-vm" {
     source = "gcs::https://www.googleapis.com/storage/v1/roi-materials/terraform"
-    project_id= "roidtc-june22-u111"
-    ip_address = "103.42.248.65"
+    project_id= "poised-cortex-357007"
+    ip_address = "34.82.122.23"
     
 }
